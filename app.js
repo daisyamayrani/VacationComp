170be6da100965f64233310df3fff492abe95d27
Daisy Pineda <amayranipineda@gmail.com>
2021-04-27 20:25:57 -0700

    made the background image change with arrow clicks on the home page

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
+// INDEX IMAGE FLIPPER
+
+const rightArrow = document.getElementsByClassName("fa-chevron-right");
+const leftArrow = document.getElementsByClassName("fa-chevron-left");
+const imgRowBg = document.getElementById("image-row");
+// const menuCircle = document.getElementById("menuCircle");
+
+let counter = 1;
+function displayImgLeft() {
+  counter++;
+  if (counter <= 4) {
+    imgRowBg.classList.add("bgImg" + counter);
+    imgRowBg.classList.remove("bgImg" + (counter - 1));
+  } else {
+    imgRowBg.classList.remove("bgImg" + (counter - 1));
+    counter = 1;
+    imgRowBg.classList.add("bgImg" + counter);
+  }
+}
+
+function displayImgRight() {
+  counter--;
+  if (counter <= 4 && counter > 0) {
+    imgRowBg.classList.add("bgImg" + counter);
+    imgRowBg.classList.remove("bgImg" + (counter + 1));
+  } else {
+    imgRowBg.classList.remove("bgImg" + (counter + 1));
+    counter = 4;
+    imgRowBg.classList.add("bgImg" + counter);
+  }
+}
+
+// END OF INDEX IMAGE FLIPPER
 
 // GALLERY PAGE JS
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
+document.body.appendChild(lightbox);
 
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
-
-
-const images = document.querySelectorAll('.photo')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
+const images = document.querySelectorAll(".photo");
+images.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    lightbox.classList.add("active");
+    const img = document.createElement("img");
+    img.src = image.src;
     while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
+      lightbox.removeChild(lightbox.firstChild);
     }
-    lightbox.appendChild(img)
-  })
-})
+    lightbox.appendChild(img);
+  });
+});
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+});
 
 //END GALLERY PAGE JS
